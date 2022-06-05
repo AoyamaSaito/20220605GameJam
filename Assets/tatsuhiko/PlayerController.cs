ecfd09ebc2f8a85cbc6c78ef80066c7bc6f65e44
tatsupiko <identity2022@outlook.jp>
2022-06-05 17:21:59 +0900

    鮭にコライダーを追加

--- Assets/tatsuhiko/PlayerController.cs
+++ Assets/tatsuhiko/PlayerController.cs
@@ -12,38 +12,60 @@ public class PlayerController : MonoBehaviour
     [SerializeField] string _clearTag = "GameClear";
 
     float _gameSpeed = 1;
+    Rigidbody2D _rb2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(CorutineTest());
+        _rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
-        rb2D.velocity = new Vector2(h * m_speed ,0);
-
+        Move();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Move()
     {
-        if(collision.gameObject.tag == _speedUpTag)
+        float h = Input.GetAxisRaw("Horizontal");
+        _rb2D.velocity = new Vector2(h * m_speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HitObject(collision, collision.gameObject.tag); 
+    }
+
+    void HitObject(Collider2D col, string hitTag)
+    {
+        if (hitTag == _speedUpTag)
         {
+            Destroy(col.gameObject);
             _gameSpeed += 0.1f;
-            _fallRiver.UpdateSpeed(_gameSpeed);
+            if (_fallRiver)
+            {
+                _fallRiver.UpdateSpeed(_gameSpeed);
+            }
         }
-        else if(collision.gameObject.tag == _speedDownTag)
+        else if (hitTag == _speedDownTag)
         {
             _gameSpeed -= 0.1f;
-            _fallRiver.UpdateSpeed(_gameSpeed);
+            if (_fallRiver)
+            {
+                _fallRiver.UpdateSpeed(_gameSpeed);
+            }
         }
-        else if (collision.gameObject.tag == _clearTag)
+        else if (hitTag == _clearTag)
         {
-            _fallRiver.StopRiver();
-            timer.TimerStop();
+            if (_fallRiver)
+            {
+                _fallRiver.UpdateSpeed(_gameSpeed);
+            }
+            if (timer)
+            {
+                timer.TimerStop();
+            }
         }
     }
 
