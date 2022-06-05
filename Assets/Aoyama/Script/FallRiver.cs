d79c0df3b880b58be496a4b0008fd88c3d6e90bb
aoyama_saito <saitomarimo1229@gmail.com>
2022-06-05 13:02:08 +0900

    [update] 川がとりあえず動くようになった

--- Assets/Aoyama/Script/FallRiver.cs
+++ Assets/Aoyama/Script/FallRiver.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallRiver : MonoBehaviour
+public class FallRiver : SingletonMonoBehaviour<FallRiver>
 {
     [SerializeField]
     private GameObject _firstRiver;
@@ -11,28 +12,57 @@ public class FallRiver : MonoBehaviour
     [SerializeField]
     private GameObject[] _goalRiver;
     [SerializeField] 
-    private float DownInterval = 30;
+    private float _downInterval = 30;
+    [SerializeField] 
+    private float _speedUpDown = 1f;
+
+    List<River> _onStageRiver = new ();
 
     private void Start()
     {
-        Instantiate(_firstRiver, Vector3.zero, Quaternion.identity)
-            .GetComponent<River>().OutAria += InstantiateRiver;
-        Instantiate(_firstRiver, new Vector3(0, DownInterval, 0), Quaternion.identity)
-            .GetComponent<River>().OutAria += InstantiateRiver;
-        Instantiate(_firstRiver, new Vector3(0, DownInterval * 2, 0), Quaternion.identity)
-            .GetComponent<River>().OutAria += InstantiateRiver;
+        Init();
+    }
+
+    private void Init()
+    {
+        _onStageRiver = new List<River>(0);
+
+        var go = Instantiate(_firstRiver, Vector3.zero, Quaternion.identity);
+        var river = go.GetComponent<River>();
+        _onStageRiver.Add(river);
+        river.OutAria += InstantiateRiver;
+
+        var go1 = Instantiate(_firstRiver, new Vector3(0, _downInterval, 0), Quaternion.identity);
+        var river1 = go1.GetComponent<River>();
+        _onStageRiver.Add(river1);
+        river1.OutAria += InstantiateRiver;
+
+        var go2 = Instantiate(_firstRiver, new Vector3(0, _downInterval * 2, 0), Quaternion.identity);
+        var river2 = go2.GetComponent<River>();
+        _onStageRiver.Add(river2);
+        river2.OutAria += InstantiateRiver;
     }
 
     private void FixedUpdate()
     {
-        
+        UpdateSpeed();
     }
 
-    
+    private void UpdateSpeed()
+    {
+        //_onStageRiver.ForEach(go => go._speed = _speedUpDown);
+    }
 
     private void InstantiateRiver()
     {
-        Instantiate(_firstRiver, transform.position, Quaternion.identity)
-            .GetComponent<River>().OutAria += InstantiateRiver;
+        var go = Instantiate(_firstRiver, transform.position, Quaternion.identity);
+        var river = go.GetComponent<River>();
+        _onStageRiver.Add(river);
+        river.OutAria += InstantiateRiver;
+    }
+
+    public void RiverRemove(River r)
+    {
+        _onStageRiver.Remove(r);
     }
 }
--- Assets/Aoyama/Script/River.cs
+++ Assets/Aoyama/Script/River.cs
@@ -7,24 +7,34 @@ public class River : MonoBehaviour
 {
     public event Action OutAria;
 
+    [HideInInspector]
+    public float _speed = 0.000001f;
+
     [SerializeField]
-    private float _destroyY = -40;
+    private float _destroyY = -15f;
 
-    float _timer = 0;
+    float _timer = 0f;
+    Transform tr;
 
-    private void Update()
+    private void Start()
+    {
+        tr = transform;
+    }
+
+    private void FixedUpdate()
     {
         Down();
     }
 
     private void Down()
     {
-        _timer += Time.deltaTime;
-        transform.position = new Vector3(transform.position.x, transform.position.y - _timer, transform.position.y);
+        float _i = 0.1f;
+        transform.position = new Vector3(tr.position.x, transform.position.y - _i, tr.position.z);
 
         if (transform.position.y <= _destroyY)
         {
             Destroy(gameObject);
+            FallRiver.Instance.RiverRemove(this);
             OutAria();
         }
     }
