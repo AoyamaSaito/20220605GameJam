553f893d6930cb491b5b8b6eaa82034d869b36e8
tatsupiko <identity2022@outlook.jp>
2022-06-05 15:58:40 +0900

    サーモンにanimationを追加 サーモンのプレハブを追加

--- Assets/tatsuhiko/PlayerController.cs
+++ Assets/tatsuhiko/PlayerController.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerContllorer : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float m_speed = 5f;
+    [SerializeField] Timer timer;
+    [SerializeField] RiverManager _fallRiver;
+    [SerializeField] float m_speed = 10f;
+    [SerializeField] string _speedUpTag = "SpeedUp";
+    [SerializeField] string _speedDownTag = "SpeedDown";
+    [SerializeField] string _clearTag = "GameClear";
+
+    float _gameSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(CorutineTest());
     }
 
     // Update is called once per frame
@@ -22,5 +30,34 @@ public class PlayerContllorer : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.tag == _speedUpTag)
+        {
+            _gameSpeed += 0.1f;
+            _fallRiver.UpdateSpeed(_gameSpeed);
+        }
+        else if(collision.gameObject.tag == _speedDownTag)
+        {
+            _gameSpeed -= 0.1f;
+            _fallRiver.UpdateSpeed(_gameSpeed);
+        }
+        else if (collision.gameObject.tag == _clearTag)
+        {
+            _fallRiver.StopRiver();
+            timer.TimerStop();
+        }
     }
+
+    //float _testSpeed = 0;
+    //IEnumerator CorutineTest() //IEmurator
+    //{
+    //  _testSpeed = 1;
+    //  yield return new WaitForSeconds(1f); //yield return new
+    //  _testSpeed = 0;
+
+    //  bool i = false;
+    //  while(i!)
+    //  {
+    //      break;
+    //  }
+    //}
 }
