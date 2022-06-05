99d6700de3150bad3b32b9246c05af47814c7eab
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2022-06-05 13:32:04 +0900

    SalmonAnimation

--- Assets/Hikanyan's_Asset/Script/Timer.cs
+++ Assets/Hikanyan's_Asset/Script/Timer.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] private float fadeTime = 1f;
+    private float countTime = 0.000f;
 
-    private float _Time;
-
-    void Start()
+    private void Start()
     {
-        
+        this.gameObject.GetComponent<CanvasGroup>().alpha = 0.500f;
     }
-
-    
     void Update()
     {
+        countTime += Time.deltaTime;
+        fadeTime -= fadeTime * Time.deltaTime;
+        GetComponent<Text>().text = countTime.ToString("F2");
         
+        colorChange();
+        
+    }
+    void colorChange()
+    {
+        this.gameObject.GetComponent<CanvasGroup>().alpha = countTime / fadeTime;
+        Debug.Log("colorChange");
     }
 }
